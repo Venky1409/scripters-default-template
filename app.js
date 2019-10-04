a97c94a8e921f1c8db25d7ebc77997e7ed538fa1
Kumar <bk185121@ncr.com>
2019-10-05 00:56:57 +0530

    Removed commented code

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// import express from "express";
-// import path from 'path';
-// import bodyParser from 'body-parser';
-// import logger from 'morgan';
-// import indexRouter from './routes/index';
-
-
-// const app = express();
-// export const routes = express.Router();
-// const port = process.env.NODE_ENV || '1409';
-// app.use(logger('app express'));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser());
-// app.use(express.static(path.join(__dirname, 'dist/')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-// indexRouter.appRoute(app);
-
-
-// // start our server on port 4201
-// app.listen(port, function() {
-//     console.log("Server now listening on port " + port);
-// });
-// export default app;
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
