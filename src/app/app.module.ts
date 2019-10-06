41b0ce85e98f33a0d624367820db905537584095
Srikanth Guntha <srikanth0190@gmail.com>
2019-10-06 23:20:46 +0530

    Committing changes of registration page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 import { RouterModule, Routes } from '@angular/router';
 import { UserIdleModule } from 'angular-user-idle';
-
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatRadioModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: RegistrationComponent}
 ];
 
 @NgModule({
@@ -26,12 +29,21 @@ const appRoutes: Routes = [
     HomeComponent,
     HeaderComponent,
     SidebarComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatCheckboxModule,
     RouterModule.forRoot(appRoutes),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
@@ -42,6 +54,7 @@ const appRoutes: Routes = [
     UserIdleModule.forRoot({idle: 0, timeout: 0, ping: 0})
   ],
   providers: [],
+  exports:[ MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
