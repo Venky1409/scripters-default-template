1f597c02ac2c63f9de3fb79c42945274590dbfd8
ShivaShankarAudam <shiva.shankar.audam@bloomingdales.com>
2019-10-07 13:12:12 +0530

    changes for refresh page issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
-import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
-import { RouterModule, Routes } from '@angular/router';
-import { UserIdleModule } from 'angular-user-idle';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatRadioModule, MatCheckboxModule } from '@angular/material';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RegistrationComponent } from './registration/registration.component';
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ToastrModule, ToastContainerModule } from "ngx-toastr";
+import { RouterModule, Routes } from "@angular/router";
+import { UserIdleModule } from "angular-user-idle";
+import { MatStepperModule } from "@angular/material/stepper";
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatRadioModule,
+  MatCheckboxModule
+} from "@angular/material";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HomeComponent } from "./home/home.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { RegistrationComponent } from "./registration/registration.component";
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'register', component: RegistrationComponent}
+  { path: "", component: HomeComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "register", component: RegistrationComponent }
 ];
 
 @NgModule({
@@ -44,18 +49,22 @@ const appRoutes: Routes = [
     MatRippleModule,
     MatRadioModule,
     MatCheckboxModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right',
+      positionClass: "toast-bottom-right",
       timeOut: 0, // disable auto-dismiss
       closeButton: true // show close button
     }),
     ToastContainerModule,
-    UserIdleModule.forRoot({idle: 0, timeout: 0, ping: 0})
+    UserIdleModule.forRoot({ idle: 0, timeout: 0, ping: 0 })
   ],
   providers: [],
-  exports:[ MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
-    
+export class AppModule {}
