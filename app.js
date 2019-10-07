eba462954d51da5d02c328af697417f3e0317425
ShivaShankarAudam <shiva.shankar.audam@bloomingdales.com>
2019-10-07 12:56:40 +0530

    changes for app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist/')));
+app.use(express.static(path.join(__dirname, "dist/")));
 
-app.get('/test', function (req, res) {
-    res.send('Testing working fine');
+app.get("/test", function(req, res) {
+  res.send("Testing working fine");
 });
 
 // Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendfile(path.join(__dirname, "index.html"));
 });
 
 indexRouter.appRoute(app);
 
 //Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
-app.listen(port, function () {
-    console.log("Server now listening on", port);
+app.listen(port, function() {
+  console.log("Server now listening on", port);
 });
