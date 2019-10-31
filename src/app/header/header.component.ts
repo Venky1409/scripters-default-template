cf2f07a1942caabb20387c7aaa6baf8298c77509
Adigicherla <venkatesh.adigicherla@spglobal.com>
2019-10-31 12:52:08 +0530

    Added restrictions to navigate pages and logout functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,14 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private toastrService: ToastrService) {
+  constructor(private toastrService: ToastrService, private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+  	sessionStorage.clear();
+  	this.router.navigate(['/login']);
   }
 }
