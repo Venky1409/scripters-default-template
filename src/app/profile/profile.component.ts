180ed93690acb256ca527116a24445b65017ac12
Adigicherla <venkatesh.adigicherla@spglobal.com>
2019-11-01 18:51:10 +0530

    profile page integration

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -20,65 +20,11 @@ export class ProfileComponent implements OnInit {
     if (!sessionStorage.length) {
       this.router.navigate(['/login']);
     } else {
-    this.profileInfo = {
-      "firstName": "shiva",
-      "lastName": "audam",
-      "userEmail": "shiva22@yopmail.com",
-      "mobile": "0888686760",
-      "dateOfBirth": "1993-02-01T18:30:00.000Z",
-      "maritalStatus": "1",
-      "homeAddress": {
-        "address1": "plot no 81, road no 7, vidyanagar, sangareddy",
-        "address2": "",
-        "city": "sangareddy",
-        "state": "4",
-        "zipcode": 502295
-      },
-      "spousedetails": {
-        "spouseFirstName": "shiva",
-        "spouseLastName": "audam",
-        "spouseEmail": "shiva@yopmail.com",
-        "spouseMobile": "0888686760",
-        "spouseDOB": "1993-02-01T18:30:00.000Z",
-        "occupation": "employed",
-        "numberOfChildren": 2,
-        "childrenList": [{
-          "name": "child1",
-          "gender": "M",
-          "grade": "1"
-        },
-        {
-          "name": "child2",
-          "gender": "F",
-          "grade": "2"
-        }
-        ]
-      },
-      "referredby": {
-        "referalFirstName": "shiva",
-        "referalLastName": "audam",
-        "referalId": "",
-        "mobile": "0888686760"
-      },
-      "businessinfo": {
-        "businessName": "bname",
-        "websiteUrl": "burl",
-        "businessDetails": "bdetails"
-      },
-      "ownbusiness": "Y",
-      "areasofinterests": [
-        "Cultural",
-        "Sports",
-        "Business"
-      ],
-      "username": "admin",
-      "password": "Admin@123"
-    };
-    this.maritalStatus = (this.profileInfo.maritalStatus === "1") ? "Married" : "Single";
     this.sessionid = sessionStorage.getItem('sessionid');
     this.registerService.getProfile(this.sessionid)
       .subscribe(res => {
-        console.log(res);
+        this.profileInfo = res.userinfo;
+        this.maritalStatus = (this.profileInfo.personal_maritalstatus === "1") ? "Married" : "Single";
       }, error => {
         console.log(error);
       });
