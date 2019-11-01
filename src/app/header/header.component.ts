5333ca09dca89a3b7371da37cd6d085395e52f7c
Adigicherla <venkatesh.adigicherla@spglobal.com>
2019-11-01 19:17:34 +0530

    Added profile details in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {ToastrService} from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,23 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private toastrService: ToastrService, private router: Router) {
+  constructor(private toastrService: ToastrService, private router: Router, private registerService: RegisterService) {
   }
 
+  profileInfo;
+
   ngOnInit() {
+  	if (!sessionStorage.length) {
+      this.router.navigate(['/login']);
+    } else {
+    this.sessionid = sessionStorage.getItem('sessionid');
+    this.registerService.getProfile(this.sessionid)
+      .subscribe(res => {
+        this.profileInfo = res.userinfo;
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 
   logout() {
