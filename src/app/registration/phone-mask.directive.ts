5031afb814586ce68f83013faf5398e6a8de7965
Adigicherla <venkatesh.adigicherla@spglobal.com>
2019-12-14 10:13:24 +0530

    fixed client issues

--- src/app/registration/phone-mask.directive.ts
+++ src/app/registration/phone-mask.directive.ts
@@ -21,7 +21,7 @@ export class PhoneMaskDirective {
   @HostListener('keydown', ['$event'])
   keydown(event) {
     event.target.maxLength = 18;
-    event.target.minLength = 16;
+    event.target.minLength = 14;
   }
   
 
@@ -35,17 +35,17 @@ export class PhoneMaskDirective {
     } else if (newVal.length <= 3) {
       newVal = newVal.replace(/^(\d{0,3})/, '($1)');
     } else if (newVal.length <= 6) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) ($2)');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1)-$2');
     } else if (newVal.length <= 10) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) ($2)-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1)-$2-$3');
     } else {
       newVal = newVal.substring(0, 11);
       let numVal = newVal.substring(0, 1);
       if (numVal == 1) {
-        newVal = newVal.replace(/^(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,4})/, '$1 ($2) ($3)-$4');
+        newVal = newVal.replace(/^(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,4})/, '$1 ($2)-$3-$4');
       } else {
         newVal = newVal.substring(0, 10);
-        newVal = newVal.replace(/^(\d{1,3})(\d{1,3})(\d{1,4})/, '($1) ($2)-$3');
+        newVal = newVal.replace(/^(\d{1,3})(\d{1,3})(\d{1,4})/, '($1)-$2-$3');
       }
     }
     this.ngControl.valueAccessor.writeValue(newVal);
